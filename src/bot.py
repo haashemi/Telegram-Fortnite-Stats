e4377a184ae5dafd88ac8ad1c8baaf9cf8c62f84
PlaySTW <60406325+PlaySTW@users.noreply.github.com>
2020-10-07 15:21:13 +0330

    Bug Fix / Change something

--- src/bot.py
+++ src/bot.py
@@ -30,17 +30,38 @@ async def get_url(params: dict) -> dict:
 
 
 class GenerateStats:
+    """
+    Class of everything that needed to make the Stats image
+    """
+
     def __init__(self) -> None:
+        """
+        Open/Ready necessary things
+        """
         self.stats = Image.open("assets/#file.png")
         self.draw = ImageDraw.Draw(self.stats)
         self.font = ImageFont.truetype(
             r"assets/fonts/BurbankBigCondensed-Black.ttf", 60)
 
     async def draw_on(self, position: tuple, text, color: tuple = None) -> None:
+        """
+        Draw some text on specific posotion of image
+
+        :param position: Position of text (x, y)
+        :param text: Any text you want to draw on image (int/float/str)
+        :param color: RGB Color of text (R, G, B)
+        """
         color = color if color is not None else (255, 255, 255)
         self.draw.text(position, str(text), color, self.font)
 
     async def get_stats(self, username, mode: str, data: dict) -> None:
+        """
+        Make an Image of player stats, main thing happens here
+
+        :param username: Username of player
+        :param mode: Game mode (overall/solo/duo/squad)
+        :param data: Collected json data from API
+        """
         await self.draw_on((140, 155), data["account"]["name"])
         await self.draw_on((785, 155),
                            str(data["battlePass"]["level"]) + "." + str(data["battlePass"]["progress"]))
@@ -73,16 +94,14 @@ class GenerateStats:
         self.stats.save(f"exports/{username}_{mode}.png")
 
 
-# +------------------+
-# |Main Menu commands|
-# +------------------+
-start_btns = [
-    [types.KeyboardButton("📊 Battle Royale Stats"),
-     types.KeyboardButton("⁉️About this bot")
-     ]
-]
+# _______________ Main Menu commands ________________
+# ___________________________________________________
+
 start_markup = types.ReplyKeyboardMarkup(
-    keyboard=start_btns, resize_keyboard=True)
+    keyboard=[[types.KeyboardButton("📊 Battle Royale Stats"),
+               types.KeyboardButton("⁉️About this bot")
+               ]],
+    resize_keyboard=True)
 
 
 @dp.message_handler(commands="start")
@@ -98,9 +117,9 @@ async def back_to_home(message: types.Message, state: FSMContext):
     await message.answer(c["t_BackToHome"], reply_markup=start_markup)
 
 
-# +-----------------+
-# | Start Menu BTNS |
-# +-----------------+
+# _________________ START Menu BTNS _________________
+# ___________________________________________________
+
 class Stats(StatesGroup):
     mode = State()
     username = State()
@@ -108,12 +127,14 @@ class Stats(StatesGroup):
 
 @dp.message_handler(lambda message: message.text == "📊 Battle Royale Stats")
 async def stats_menu(message: types.Message):
-    modes_btns = [
-        [types.KeyboardButton("☑️ Overall"), types.KeyboardButton("🔴 Solo")],
-        [types.KeyboardButton("🔵 Duos"), types.KeyboardButton("🟢 Squads")]
-    ]
     modes_markup = types.ReplyKeyboardMarkup(
-        keyboard=modes_btns, resize_keyboard=True)
+        keyboard=[
+            [types.KeyboardButton("☑️ Overall"),
+             types.KeyboardButton("🔴 Solo")],
+            [types.KeyboardButton("🔵 Duos"),
+             types.KeyboardButton("🟢 Squads")]
+        ], resize_keyboard=True)
+
     await message.answer(c["t_BRStats"], reply_markup=modes_markup)
     await Stats.mode.set()
 
@@ -123,24 +144,26 @@ async def about_bot(message: types.Message):
     await message.answer(c["t_AboutBot"])
 
 
-# +-----------------+
-# | Start Menu BTNS |
-# +-----------------+
+# _________________ STATS Menu BTNS _________________
+# ___________________________________________________
 @dp.message_handler(state=Stats.mode)
-async def mode_state(message: types.Message, state: FSMContext):
+async def select_mode(message: types.Message, state: FSMContext):
     mode = {
         "☑️ Overall": "overall",
         "🔴 Solo": "solo",
         "🔵 Duos": "duo",
         "🟢 Squads": "squad"
     }
+
+    back_btn = types.ReplyKeyboardMarkup(
+        keyboard=[[types.KeyboardButton("↩️ Back to home")]],
+        resize_keyboard=True)
+
+    # If sended message was in dict (☑️ Overall/ 🔴 Solo/ 🔵 Duos/ 🟢 Squads)
     if message.text in mode:
-        await message.answer(
-            c["t_SendUsername"],
-            reply_markup=types.ReplyKeyboardMarkup(
-                keyboard=[[types.KeyboardButton("↩️ Back to home")]],
-                resize_keyboard=True)
-        )
+        await message.answer(c["t_SendUsername"], reply_markup=back_btn)
+
+        # Proxy the content to the next state
         async with state.proxy() as data:
             data["mode"] = mode[message.text]
             await Stats.next()
@@ -151,23 +174,33 @@ async def mode_state(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state=Stats.username)
 async def username_state(message: types.Message, state: FSMContext):
+    """
+    Here is the main part of the bot
+    First, it gets the username from the message,
+    Then use proxied content (mode) from states
+    then it will do something from responses with requesting to the API
+    """
     async with state.proxy() as data:
-        account_params = {"name": str(
-            message.text), "accountType": "epic", "timeWindow": "lifetime"}
+        # Get request to the API by passing some parameters
+        await state.finish()
+        account_params = {"name": str(message.text),
+                          "accountType": "epic", "timeWindow": "lifetime"}
         resp = await get_url(account_params)
 
-        if resp["status"] == 404:
-            await message.answer(c["t_Status404"], reply_markup=start_markup)
-        elif resp["status"] == 403:
-            await message.answer(c["t_Status403"], reply_markup=start_markup)
-        elif resp["status"] == 200:
-            generator = GenerateStats()
+        # Do some thing with response code from API
+        if resp["status"] == 404:  # It means username not Fount
+            with open(f"assets/#not_found.png", "rb") as not_found:
+                await message.answer_photo(not_found, c["t_Status404"], reply_markup=start_markup)
+        elif resp["status"] == 403:  # It means user stats is private
+            with open(f"assets/#private.png", "rb") as private:
+                await message.answer_photo(private, c["t_Status403"], reply_markup=start_markup)
+        elif resp["status"] == 200:  # It means everything should works fine
+            generator = GenerateStats()  # Open #File.png then generate stats
             await generator.get_stats(message.text, data['mode'], resp["data"])
             with open(f"exports/{message.text}_{data['mode']}.png", "rb") as stats:
                 await message.answer_photo(stats, c["t_Status200"].format(message.text), reply_markup=start_markup)
-        else:
-            await message.answer(c["t_StatusERR"], reply_markup=start_markup)
-        await state.finish()
+        elif resp["status"] == 429:  # It means you requested more than maximum allowed requests
+            await message.answer(c["t_Status429"], reply_markup=start_markup)
 
 
 if __name__ == "__main__":
